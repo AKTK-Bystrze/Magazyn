8dd5b334ed188e999ae320a937aca448ff5a537f
MateuszGasior <mateo.g6@gmail.com>
2023-08-04 16:50:08 +0200

    make test skipped

--- emailService_test.go
+++ emailService_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 func TestEmailService(t *testing.T) {
+	t.Skip("SKIP TEST")
 	receiver := User{
 		ID:    1,
 		Name:  "Name",
 		Email: MAGAZYN_BYSTRZE_EMAIL,
 	}
-	result := SendEmail(receiver, "Subject", "This is a test message, please ignore.")
+	result := SendEmail(receiver, "Test email", "This is a test message, please ignore.")
 	if result != nil {
 		t.Errorf("Can't send test email, error: %s", result)
 	}
