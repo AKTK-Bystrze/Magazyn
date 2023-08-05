bda1f4b3d3dce960b95c64c28ec767f123f6b52b
MateuszGasior <mateo.g6@gmail.com>
2023-08-05 12:25:26 +0200

    code clean up

--- auth.go
+++ auth.go
@@ -1,70 +1,69 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 )
 
 type tmpUser struct {
-  ID     int64    `db:"u_id"`
-  Name   string   `db:"u_username"`
-  Role   string   `db:"u_role"`
+	ID   int64  `db:"u_id"`
+	Name string `db:"u_username"`
+	Role string `db:"u_role"`
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-    //  TODO: check if user is logged in
-    // display the login form
-    if r.Method == "GET" {
-        app.renderTemplateNoData(w, "login.html")
-        return
-    }
+	//  TODO: check if user is logged in
+	// display the login form
+	if r.Method == "GET" {
+		app.renderTemplateNoData(w, "login.html")
+		return
+	}
 
-    // get the username and password from the request
-    username := r.FormValue("username")
-    password := r.FormValue("password")
-    password = password
+	// get the username and password from the request
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	password = password
 
-    session, _ := app.store.Get(r, SESSION_NAME)
-    target := "/dashboard"
+	session, _ := app.store.Get(r, SESSION_NAME)
+	target := "/dashboard"
 
-    var u tmpUser
-    err := app.db.Get(&u, "SELECT u_username, u_id, u_role FROM users WHERE u_username = ?", username)
-    if err != nil {
-      app.Err(err.Error())
-      // if the username or password is invalid, display an error message
-      err := app.templates.ExecuteTemplate(w, "login.html", struct {
-        Msg string
-      }{
-        Msg: "Invalid username or password",
-      })
-      if err != nil {
-					app.Err(err.Error())
-          http.Error(w, "Template error", http.StatusInternalServerError)
-          return
-      }
-      return
-    }
+	var u tmpUser
+	err := app.db.Get(&u, "SELECT u_username, u_id, u_role FROM users WHERE u_username = ?", username)
+	if err != nil {
+		app.Err(err.Error())
+		// if the username or password is invalid, display an error message
+		err := app.templates.ExecuteTemplate(w, "login.html", struct {
+			Msg string
+		}{
+			Msg: "Invalid username or password",
+		})
+		if err != nil {
+			app.Err(err.Error())
+			http.Error(w, "Template error", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
 
-    session.Values["UserInfo"] = int(u.ID)
+	session.Values["UserInfo"] = int(u.ID)
 
-    if u.Role == "admin" {
-        target = "/admin/reservations"
-    } 
+	if u.Role == "admin" {
+		target = "/admin/reservations"
+	}
 
-    err = session.Save(r, w)
-    if err != nil {
-      app.Err(err.Error())
-    }
-    // redirect to the user dashboard
-    http.Redirect(w, r, target, http.StatusSeeOther)
-    return
+	err = session.Save(r, w)
+	if err != nil {
+		app.Err(err.Error())
+	}
+	// redirect to the user dashboard
+	http.Redirect(w, r, target, http.StatusSeeOther)
+	return
 }
 
 func Logout(w http.ResponseWriter, r *http.Request) {
-    session, _ := app.store.Get(r, SESSION_NAME)
-    for key := range session.Values {
-      delete(session.Values, key)
-    }
-    session.Save(r, w)
-    http.Redirect(w, r, "/", http.StatusSeeOther)
+	session, _ := app.store.Get(r, SESSION_NAME)
+	for key := range session.Values {
+		delete(session.Values, key)
+	}
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-
