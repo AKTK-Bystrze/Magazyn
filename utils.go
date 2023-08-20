96ab2c88621637864e451e7ebe4c701871a05931
GasiorMateusz <mateuszgasior314@gmail.com>
2023-08-20 10:43:20 +0200

    add passwordless interface

--- utils.go
+++ utils.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gorilla/sessions"
+	"github.com/johnsto/go-passwordless/v2"
 )
 
 // Error represents an error that is displayed to the user.
@@ -32,4 +36,17 @@ func isSignedIn(s *sessions.Session) bool {
 func getEmailUsername(email string) string {
 	usernameAndDomain := strings.Split(email, "@")
 	return usernameAndDomain[0]
+}
+
+type Templates interface {
+	ExecuteTemplate(wr io.Writer, name string, data any) error
+}
+
+type Passwordless interface {
+	GetStrategy(ctx context.Context, name string) (passwordless.Strategy, error)
+	ListStrategies(ctx context.Context) map[string]passwordless.Strategy
+	RequestToken(ctx context.Context, s string, uid string, recipient string) error
+	SetStrategy(name string, s passwordless.Strategy)
+	SetTransport(name string, t passwordless.Transport, g passwordless.TokenGenerator, ttl time.Duration) passwordless.Strategy
+	VerifyToken(ctx context.Context, uid string, token string) (bool, error)
 }
