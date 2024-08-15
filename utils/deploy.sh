b0697279406d3f501bfcc8517b30071a25c3a2c7
GasiorMateusz <mateuszgasior314@gmail.com>
2024-08-15 12:32:32 +0200

    add deploy script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,5 +1,5 @@
 # build, push image to google cloud and deploy app
-docker build -t gcr.io/magazynbystrze/app -t magazyn_bystrze . --build-arg EMAIL=test.dev.g6@gmail.com --build-arg EMAIL_PASS="" 
+docker build -t gcr.io/magazynbystrze/app -t magazyn_bystrze . --build-arg EMAIL=test.dev.g6@gmail.com --build-arg EMAIL_PASS="ushd dvvv jmiy wgeq" 
 gcloud auth configure-docker
 docker push gcr.io/magazynbystrze/app
 gcloud run deploy app --image gcr.io/magazynbystrze/app --platform managed --region europe-central2 --allow-unauthenticated
