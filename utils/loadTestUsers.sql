bffd31f5559990e453b8eaa2d17c09651b00dc0e
GasiorMateusz <mateuszgasior314@gmail.com>
2024-08-18 22:45:54 +0200

    add scratch folder to gitignore

--- utils/loadTestUsers.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (u_id, u_username, u_role, u_email, u_credits) VALUES
-(0, 'kursant2', 'user', 'dummy1@op.pl', 10),
-(1, 'kursant1', 'user', 'mateo.g6@gmail.com', 200),
-(2, 'admin1', 'admin', 'mateuszgasior314@gmail.com', 200),
-(3, 'admin2', 'admin', 'kamilgrzejszczyk13@gmail.com', 200),
-(4, 'admin3', 'admin', 'dummy3@email.com', 10);
