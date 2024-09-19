ae12819fb12047030851807288dd991b338fe4cf
GasiorMateusz <mateuszgasior314@gmail.com>
2024-09-19 20:41:06 +0200

    turn off debug mode

--- structs.go
+++ structs.go
@@ -73,7 +73,7 @@ var PRIVILIGES = []string{ADMIN, NINJA, USER, SUPERADMIN}
 
 const (
 	COOKIE_VALIDITY_TIME_HOURS = 6
-	SEND_COOKIE_TO_STDOUT      = true
+	SEND_COOKIE_TO_STDOUT      = false
 	TOKEN_LENGTH               = 10
 	COOKIE_KEY_LENGTH          = 16
 
