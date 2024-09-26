0d754041c8e0ba70db0f8004935bde5c896c66b9
GasiorMateusz <mateuszgasior314@gmail.com>
2024-09-26 09:22:03 +0200

    refactor session

--- src/apps/common/session/session.go
+++ src/apps/common/session/session.go
@@ -2,26 +2,20 @@ package session
 
 import (
 	"bystrze/apps/common/models"
-	"github.com/gorilla/sessions"
 	"net/http"
+
+	"github.com/gorilla/sessions"
 )
-
-func GetSessionUserName(r *http.Request) string {
-	uinfo, ok := r.Context().Value("UserInfo").(models.User)
-	return If(ok, uinfo.Name, "unknown")
-}
-
-func If[T any](cond bool, vtrue, vfalse T) T {
-	if cond {
-		return vtrue
-	}
-	return vfalse
-}
 
 type SessionStore interface {
 	Get(r *http.Request, name string) (*sessions.Session, error)
 	New(r *http.Request, name string) (*sessions.Session, error)
 	Save(r *http.Request, w http.ResponseWriter, s *sessions.Session) error
+}
+
+func GetSessionUserName(r *http.Request) string {
+	uinfo, ok := r.Context().Value("UserInfo").(models.User)
+	return If(ok, uinfo.Name, "unknown")
 }
 
 func GetSessionUserId(r *http.Request) int64 {
@@ -31,4 +25,11 @@ func GetSessionUserId(r *http.Request) int64 {
 
 func IsSignedIn(s *sessions.Session) bool {
 	return s != nil && s.Values["UserInfo"] != nil
+}
+
+func If[T any](cond bool, vtrue, vfalse T) T {
+	if cond {
+		return vtrue
+	}
+	return vfalse
 }
