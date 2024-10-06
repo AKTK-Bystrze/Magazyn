7ffa103bc9608be0bd4a10cacd3737687ee3201c
GasiorMateusz <mateuszgasior314@gmail.com>
2024-10-06 09:41:17 +0200

    add rainny and sanny scenarios reservations

---
+++ boxTest/sunnyReservationScenario_test.go
@@ -0,0 +1,25 @@
+package main
+
+//user search for a item
+//user reserve item for today
+// - check db reservation status
+// - check db user's credits
+// - check item availability
+//admin approves item the same day
+// - chek db reservation status
+//admin gives item the same day
+// - check db reservation status
+//user return item the selected day
+// - check db user's credits
+// - check db reservation status
+//addmin approves item return
+// - check db user's credits
+// - check db reservation status
+// - check item availability
+
+//cases:
+//1 day reservation
+//2 weekend reservation
+//1 wekk reservation
+//reservation is in the future
+//reservation is ended quicker
