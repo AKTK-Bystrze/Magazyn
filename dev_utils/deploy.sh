1240ba953dd96ed349bcca0f12b8201abfdf7033
GasiorMateusz <mateuszgasior314@gmail.com>
2024-10-09 21:51:14 +0200

    update deploy.sh

--- dev_utils/deploy.sh
+++ dev_utils/deploy.sh
@@ -1,8 +1,14 @@
-# build, push image to google cloud and deploy app
-docker build --target production -t gcr.io/magazynbystrze/app -t magazyn_bystrze . --build-arg EMAIL=test.dev.g6@gmail.com --build-arg EMAIL_PASS="ushd dvvv jmiy wgeq" 
+# test app
+go clean -testcache
+go cd boxTest
+go run main.go --setUp
+go go run main.go --tests 
+    #OPTIONAL
+    #create and populate db. Can get current one from a server using admin's /db/backup endpoint
+    # sqlite3 magazyn_prod.db < db.schema
+    # sqlite3 magazyn_prod.db ".read boxTest/db_test.data" 
+    # build, push image to google cloud and deploy app
+docker build --target production -t gcr.io/magazynbystrze/app -t magazyn_bystrze . --build-arg EMAIL=EMAIL --build-arg EMAIL_PASS="PASS" DB_PATH=./magazyn_prod.db
 gcloud auth configure-docker
 docker push gcr.io/magazynbystrze/app
 gcloud run deploy app --image gcr.io/magazynbystrze/app --platform managed --region europe-central2 --allow-unauthenticated
-#create and populate db. Can get current one from a server using admin's /db/backup endpoint
-sqlite3 magazyn.db < db.schema
-sqlite3 magazyn.db ".read boxTest/db_test.data"
