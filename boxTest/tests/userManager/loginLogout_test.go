79c0f40df7bd1f40664f8be4c3409fa6b75f4ec5
GasiorMateusz <mateuszgasior314@gmail.com>
2024-10-10 13:30:18 +0200

    add Test_allUsers_loginSameTime

--- boxTest/tests/userManager/loginLogout_test.go
+++ boxTest/tests/userManager/loginLogout_test.go
@@ -19,11 +19,34 @@ func loginThenLogout(user app.User) error {
 	return uc.LogOut()
 }
 
-func Test_allUsers(t *testing.T) {
+func Test_allUsers_loginAndlogut(t *testing.T) {
 	for _, user := range db.USERS_MAP {
 		err := loginThenLogout(user)
 		if err != nil {
 			t.Errorf("Login and logout for %v failed %v", user.Name, err)
+		}
+	}
+}
+
+func Test_allUsers_loginSameTime(t *testing.T) {
+	for _, user := range db.USERS_MAP {
+		uc := app.UserClient{
+			User:   user,
+			Client: app.CreateHttpClient(),
+		}
+		err := uc.Login()
+		if err != nil {
+			t.Errorf("Login for %v failed %v", user.Name, err)
+		}
+	}
+	for _, user := range db.USERS_MAP {
+		uc := app.UserClient{
+			User:   user,
+			Client: app.CreateHttpClient(),
+		}
+		err := uc.LogOut()
+		if err != nil {
+			t.Errorf("Logout for %v failed %v", user.Name, err)
 		}
 	}
 }
