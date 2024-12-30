8acf9725b66dd2d9f72ff2428bc91fcab2117078
GasiorMateusz <mateuszgasior314@gmail.com>
2024-12-30 20:18:19 +0100

    refactor dockerfiles

--- db/sqlite/db_populate.sh
+++
@@ -1,3 +0,0 @@
-#create and populate db. Can get current one from a server using admin's /db/backup endpoint
-sqlite3 magazyn_prod.db < db.schema
-sqlite3 magazyn_prod.db ".read boxTest/db_test.data" 
