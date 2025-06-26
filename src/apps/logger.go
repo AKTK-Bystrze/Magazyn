21c096f85ae206983860f97182e63b54838c5359
Krzysztof Ziobro <jerzym27@protonmail.com>
2025-06-26 20:50:26 +0200

    Change log format

--- src/apps/logger.go
+++ src/apps/logger.go
@@ -8,27 +8,27 @@ import (
 )
 
 func (app *App) SetLogger() {
-	app.Logger = log.New(os.Stdout, app.AppName+":\t", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
+	app.Logger = log.New(os.Stdout, app.AppName+"\t", log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
 func (app App) Debug(format string, a ...any) {
-	app.Logger.Printf("DEBUG: "+format, a...)
+	app.Logger.Printf("DEBUG:\t"+format, a...)
 }
 
 func (app App) Info(format string, a ...any) {
-	app.Logger.Printf("INFO: "+format, a...)
+	app.Logger.Printf("INFO:\t"+format, a...)
 }
 
 func (app App) Warn(format string, a ...any) {
-	app.Logger.Printf("WARN: "+format, a...)
+	app.Logger.Printf("WARN:\t"+format, a...)
 }
 
 func (app App) Err(format string, a ...any) {
-	app.Logger.Printf("ERR: "+format, a...)
+	app.Logger.Printf("ERR:\t"+format, a...)
 }
 
 func (app App) ErrSession(r *http.Request, e error) {
-	app.Logger.Printf("ERR: %v %v", session.GetSessionUserName(r), e.Error())
+	app.Logger.Printf("ERR:\t%v %v", session.GetSessionUserName(r), e.Error())
 }
 
 func (app App) Fatal(v ...any) {
