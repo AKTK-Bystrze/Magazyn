7e791078e969e25e002bcbfc80365b1cc929372f
GasiorMateusz <mateuszgasior314@gmail.com>
2025-09-23 17:56:19 +0200

    remove wait in bxt test env preparation

--- boxTest/env/env.go
+++ boxTest/env/env.go
@@ -46,8 +46,6 @@ func composeContainers() {
 		log.Printf("Error running Docker Compose: %v\n", err)
 		os.Exit(1)
 	}
-	log.Printf("WAIT for app to deploy...")
-	time.Sleep(time.Second * 10)
 	log.Print("App deployed on " + Localhost)
 	log.Printf("To log to application use one of the users from the xdata.sql. Try `superAdmin`. Login link is inside the %s container", TEST_APP_NAME)
 	log.Printf("to run all tests type: run main.go --tests")
