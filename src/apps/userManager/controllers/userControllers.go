cb1549878f3f1c4a8927cf0ffa43195685a73f45
GasiorMateusz <111558530+GasiorMateusz@users.noreply.github.com>
2025-09-30 19:17:53 +0200

    75 users manager view (#91)
    * update userManager view for updating users
    
    * fix value sign
    
    * update users manager view

--- src/apps/userManager/controllers/userControllers.go
+++ src/apps/userManager/controllers/userControllers.go
@@ -5,15 +5,15 @@ import (
 	"bystrze/apps/common/contextHelpers"
 	"bystrze/apps/common/models"
 	"bystrze/apps/common/session"
-	"bystrze/apps/common/timeSet"
 	"bystrze/apps/userManager/appState"
 	"bystrze/apps/userManager/auth/access"
 	"bystrze/apps/userManager/credits"
 	"bystrze/apps/userManager/users"
 	"bystrze/apps/warehouse/rental"
 	"net/http"
-	"strconv"
 	"time"
+	"bystrze/apps/common/timeSet"
+	"strconv"
 )
 
 func UserDashboard(w http.ResponseWriter, r *http.Request) {
@@ -40,86 +40,165 @@ func UserDashboard(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		appState.App.Err("%v Form parsing error %v", session.GetSessionUserName(r), err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
+	// 1. Authorization check (must be admin/superadmin)
 
-	userID, err := strconv.Atoi(r.FormValue("ID"))
-	if err != nil {
-		appState.App.Err("%v Form parsing error %v", session.GetSessionUserName(r), err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	user, err := users.GetUserById(userID)
-	if err != nil {
-		appState.App.Err("%v Can't get user %v", session.GetSessionUserName(r), err)
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	tmpCredits := r.FormValue("credits")
-	var audit models.CreditsAudit
-	var newCredits int
-	if tmpCredits != "" {
-		newCredits, err = strconv.Atoi(tmpCredits)
+    if r.Method != "POST" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    r.ParseForm()
+
+    // 'actionType' identifies which form was submitted: 'singleCredit', 'singleRole', or 'bulkCredit'
+    actionType := r.PostFormValue("actionType")
+
+    switch actionType {
+	case "singleEnabled":
+        userID, _ := strconv.Atoi(r.PostFormValue("userID"))
+        newEnabled, err := strconv.ParseBool(r.PostFormValue("newEnabledState"))
 		if err != nil {
-			appState.App.Err("%v Form parsing error %v", session.GetSessionUserName(r), err)
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "Invalid enabled state", http.StatusBadRequest)
 			return
 		}
-		audit = models.CreditsAudit{
+        
+        user, err := users.GetUserById(userID) // Fetch user
+        if err != nil { 
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "DB Error", http.StatusInternalServerError)
+			return
+		 }
+
+        user.Enabled = newEnabled
+		err = updateUser(w, r, user)
+		if err != nil {
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "DB Error", http.StatusInternalServerError)
+			return
+		}
+		appState.App.Info("%v updated user %v enabled to %v", session.GetSessionUserName(r), user.Name, user.Enabled)
+
+    case "singleCredit":
+		userID, _ := strconv.Atoi(r.PostFormValue("userID"))
+        amount, _ := strconv.Atoi(r.PostFormValue("amount"))
+        action := r.PostFormValue("action")
+        
+        user, err := users.GetUserById(userID)
+        if err != nil { 
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "DB Error", http.StatusInternalServerError)
+			return
+		 }
+		value := 0
+        switch action {
+			case "add":
+				user.Credits += amount
+				value = amount
+			case "subtract":
+				user.Credits -= amount
+				value = -amount
+        }
+			audit := models.CreditsAudit{
+				U_ID:        int(user.ID),
+				Author_ID:   int(session.GetSessionUserId(r)),
+				Value:       value,
+				Balance:     user.Credits,
+				Description: "Edycja",
+				ChangeDate:  time.Now().In(timeSet.LOCATION),
+		}
+		
+			updateUserCredits(w, r, user, audit) // Save user to DB using existing function
+        
+    case "singleRole":
+        // Logic for single user role change
+        userID, _ := strconv.Atoi(r.PostFormValue("userID"))
+        newRole := r.PostFormValue("newRole")
+        
+        user, err := users.GetUserById(userID) // Fetch user
+        if err != nil { 
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "DB Error", http.StatusInternalServerError)
+			return
+		 }
+
+        user.Role = newRole
+		err = updateUser(w, r, user)
+		if err != nil {
+			appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+			http.Error(w, "DB Error", http.StatusInternalServerError)
+			return
+		}
+		appState.App.Info("%v updated user %v role to %v", session.GetSessionUserName(r), user.Name, user.Role)
+		if user.ID == session.GetSessionUserId(r) {
+		appState.App.Debug("%v user requested changes for his own role, relogin is needed", session.GetSessionUserName(r))
+		Logout(w, r)
+	}
+
+    case "bulkCredit":
+        // Logic for bulk credit add/subtract
+        selectedIDs := r.PostForm["selectedUserIDs"] // Slice of user IDs
+        amount, _ := strconv.Atoi(r.PostFormValue("amount"))
+        action := r.PostFormValue("action")
+        
+		for _, idStr := range selectedIDs {
+			userID, _ := strconv.Atoi(idStr)
+			user, err := users.GetUserById(userID)
+			if err != nil { 
+				appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
+				continue // Skip this user and continue with others
+			}
+			value := 0
+			switch action {
+				case "add":
+					user.Credits += amount
+					value = amount
+				case "subtract":
+					user.Credits -= amount
+					value = -amount
+			}
+			audit := models.CreditsAudit{
 			U_ID:        int(user.ID),
 			Author_ID:   int(session.GetSessionUserId(r)),
-			Value:       newCredits - user.Credits,
-			Balance:     newCredits,
+			Value:       value,
+			Balance:     user.Credits,
 			Description: "Edycja",
 			ChangeDate:  time.Now().In(timeSet.LOCATION),
 		}
-		user.Credits = newCredits
-	}
-	userRole := r.FormValue("role")
-	if userRole != "" {
-		if !access.AreRolesValid(userRole) {
-			appState.App.Err("%v invalid new roles", session.GetSessionUserName(r))
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-			return
+			updateUserCredits(w, r, user, audit) // Save user to DB using existing function
 		}
-		user.Role = userRole
-	}
-	userEnabled := r.FormValue("enabled")
-	switch userEnabled {
-	case "on":
-		user.Enabled = true
-	case "":
-		user.Enabled = false
-	default:
-		appState.App.Err("%v invalid enbaled value", session.GetSessionUserName(r))
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
+
+    default:
+        http.Error(w, "Invalid action type", http.StatusBadRequest)
+        return
+    }
+    
+    w.WriteHeader(http.StatusOK)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request, user models.User) error {
 	appState.App.Debug("%v Requested update of user: %v", session.GetSessionUserName(r), user.Name)
-	err = users.UpdateUser(user)
+	err := users.UpdateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateUserCredits(w http.ResponseWriter, r *http.Request, user models.User, audit models.CreditsAudit) {
+	appState.App.Debug("%v Requested update of user: %v", session.GetSessionUserName(r), user.Name)
+	err := users.UpdateUser(user)
 	if err != nil {
 		appState.App.Err("%v %v", session.GetSessionUserName(r), err.Error())
 		http.Error(w, "DB error", http.StatusBadRequest)
 		return
 	} else {
 		if audit != (models.CreditsAudit{}) {
-			// No point in failing the request now, since we already modified db state.
-			// TODO: this should be a part of the same db transaction as credit update or be added by a db trigger.
 			if err := credits.InsertCreditsAudit(audit); err != nil {
 				appState.App.Err("Error %v encountered when inserting credit audit info into db - db might be in an inconsistent state.", err)
 			}
 		}
 	}
-	if user.ID == session.GetSessionUserId(r) {
-		appState.App.Debug("%v user requested changes for his own role, relogin is needed", session.GetSessionUserName(r))
-		Logout(w, r)
-	}
-	appState.App.Debug("%v updated user %v credits to %v roles to %v enabled to %v", session.GetSessionUserName(r), user.Name, user.Credits, user.Role, user.Enabled)
-	w.WriteHeader(http.StatusOK)
+	appState.App.Debug("%v updated user %v credits to %v", session.GetSessionUserName(r), user.Name, user.Credits)
 }
 
 func GetUsersController(w http.ResponseWriter, r *http.Request) {
@@ -132,8 +211,10 @@ func GetUsersController(w http.ResponseWriter, r *http.Request) {
 
 	appState.App.RenderTemplate(w, r, "users.html", &struct {
 		Users []models.User
+		Roles []string
 		apps.TemplateData
 	}{
 		Users: users,
+		Roles: access.ROLES,
 	})
 }
