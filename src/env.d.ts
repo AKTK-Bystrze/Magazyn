afaed2f415ae72cfa229dffa153bb82f33027f39
GasiorMateusz <mateuszgasior314@gmail.com>
2025-11-29 10:44:14 +0100

    rename envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -12,10 +12,8 @@ declare global {
 }
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string;
-  readonly SUPABASE_KEY: string;
-  readonly PUBLIC_SUPABASE_URL: string;
-  readonly PUBLIC_SUPABASE_ANON_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
 }
 
 interface ImportMeta {
