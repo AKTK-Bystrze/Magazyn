5ad7276eb3ce8a9168cd7403704715a967fe554c
GasiorMateusz <mateuszgasior314@gmail.com>
2026-01-01 19:32:50 +0100

    style: pr title validation error message

--- frontend/commitlint.config.js
+++ frontend/commitlint.config.js
@@ -1,7 +1,7 @@
 export default {
     extends: ['@commitlint/config-conventional'],
     helpUrl: `
-Common types: feat | fix | docs | refactor | test | chore | ci | perf | style | build
+Common types: feat | fix | docs | refactor | test | chore | ci | perf | style | build | revert
 Example: feat(auth): add OAuth support
 
 Full guide: https://www.conventionalcommits.org/`,
